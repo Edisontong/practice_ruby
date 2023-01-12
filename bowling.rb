32f0287001f5bd620c0c7bd633329e798b0ac3c3
Edisontong <edisonjtong@gmail.com>
2023-01-12 09:34:29 -0700

    commented out previous code. experiemanting with new code to try and make it more clear

--- bowling.rb
+++ bowling.rb
@@ -1,33 +1,61 @@
-@score = 0
-@game = []
-@frame = []
-@index = 0
-@round = 1
+# @score = 0
+# @game = []
+# @frame = []
+# @index = 0
+# @round = 1
+
+# def roll
+#   puts "Roll 1:"
+#   @roll = gets.chomp.to_i
+#   if @roll == 10
+#     @frame << @roll
+#     @frame << 0
+#   else
+#     @frame << @roll
+#     puts "Roll 2:"
+#     @roll = gets.chomp.to_i
+#     @frame << @roll
+#   end
+#   @game << @frame
+#   p @game
+# end
+
+# def score
+#   @score = @score + @frame[0] + @frame[1]
+# end
+
+# 10.times do
+#   puts "FRAME: #{@index}"
+#   roll
+
+#   score
+#   puts "TOTAL: #{@score}"
+# end
+
+@score = []
 
 def roll
-  puts "Roll 1:"
-  @roll = gets.chomp.to_i
-  if @roll == 10
-    @frame << @roll
-    @frame << 0
+  puts "First roll:"
+  roll1 = gets.chomp.to_i
+  if roll1 == 10
+    roll2 = 0
   else
-    @frame << @roll
-    puts "Roll 2:"
-    @roll = gets.chomp.to_i
-    @frame << @roll
+    puts "Second roll:"
+    roll2 = gets.chomp.to_i
   end
-  @game << @frame
-  p @game
+  @score << [roll1, roll2]
 end
 
 def score
-  @score = @score + @frame[0] + @frame[1]
+  index = 0
+  while index < @score.length
+    if @score[index][0]
+    end
+    index += 1
+  end
 end
 
 10.times do
-  puts "FRAME: #{@index}"
   roll
-
-  score
-  puts "TOTAL: #{@score}"
+  p @score
 end
