33d1d659a2cf61613485cc70e3420c810b18a8dc
Edisontong <edisonjtong@gmail.com>
2023-01-12 09:33:17 -0700

    roll method and score method. mmade loop to run ten times to simulate bowling

--- bowling.rb
+++ bowling.rb
@@ -1,1 +1,33 @@
+@score = 0
+@game = []
+@frame = []
+@index = 0
+@round = 1
 
+def roll
+  puts "Roll 1:"
+  @roll = gets.chomp.to_i
+  if @roll == 10
+    @frame << @roll
+    @frame << 0
+  else
+    @frame << @roll
+    puts "Roll 2:"
+    @roll = gets.chomp.to_i
+    @frame << @roll
+  end
+  @game << @frame
+  p @game
+end
+
+def score
+  @score = @score + @frame[0] + @frame[1]
+end
+
+10.times do
+  puts "FRAME: #{@index}"
+  roll
+
+  score
+  puts "TOTAL: #{@score}"
+end
