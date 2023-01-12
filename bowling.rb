2a26d494fa211e084f5c5ec90fd2ab0123d6c610
Edisontong <edisonjtong@gmail.com>
2023-01-12 13:25:13 -0700

    made it so you can roll a third time on the last roll if you get a strike or a spare. scoring still not working properly

--- bowling.rb
+++ bowling.rb
@@ -37,25 +37,52 @@
 def roll
   puts "First roll:"
   roll1 = gets.chomp.to_i
-  if roll1 == 10
+  if @score.length == 9
+    if roll1 == 10
+      puts "Second roll:"
+      roll2 = gets.chomp.to_i
+      puts "Third roll:"
+      roll3 = gets.chomp.to_i
+      @score << [roll1, roll2, roll3]
+    end
+  elsif roll1 == 10
+    roll2 = 0
     @score << [roll1]
   else
     puts "Second roll:"
     roll2 = gets.chomp.to_i
-    @score << [roll1, roll2]
+    if roll1 + roll2 == 10
+      puts "Third roll:"
+      roll3 = gets.chomp.to_i
+      @score << [roll1, roll2, roll3]
+    else
+      @score << [roll1, roll2]
+    end
   end
 end
 
 def score
+  total = 0
   index = 0
-  while index < @score.length
-    if @score[index][0] == 10
+  while index < @score.length - 1
+    if @score[index].length == 2
+      total = total + @score[index][0] + @score[index][1]
+    else
+      total = total + @score[index][0]
+      if @score[index + 1].length == 2
+        total = total + @score[index + 1][0] + @score[index + 1][1]
+      else
+        total = total + @score[index + 1][0] + @score[index + 2][0]
+      end
     end
     index += 1
   end
+  p total
 end
 
 10.times do
   roll
   p @score
 end
+
+score
